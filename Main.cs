00d4e5fe5980730aae608b06533fd3426b86b00b
Raphdf201 <raphdf201@hotmail.com>
2025-11-06 07:27:46 -0500

    start working on mod

--- Main.cs
+++ Main.cs
@@ -1,7 +1,35 @@
-﻿using System;
+﻿using Core.Localization;
+using ShapezShifter.Flow;
+using ShapezShifter.Flow.Atomic;
+using ShapezShifter.Kit;
+using ShapezShifter.Textures;
+using ILogger = Core.Logging.ILogger;
 
 namespace MoreLogic;
 
-public class Main
+public class Main : IMod
 {
+    public Main(ILogger logger)
+    {
+        BuildingDefinitionGroupId groupId = new("MoreLogicGroup");
+        BuildingDefinitionId nandDefId = new("NAND");
+
+        const string nandTitleId = "building-variant.nand.title";
+        const string nandDescriptionId = "building-variant.nand.description";
+
+        var modResourcesLocator = ModDirectoryLocator.CreateLocator<Main>().SubLocator("Resources");
+
+        using var assetBundleHelper = AssetBundleHelper.CreateForAssetBundleEmbeddedWithMod<Main>("Resources/bundle");
+        
+        var iconPath = modResourcesLocator.SubPath("icon.png");
+
+        var groupBuilder = BuildingGroup.Create(groupId)
+            .WithTitle(nandTitleId.T())
+            .WithDescription(nandDescriptionId.T())
+            .WithIcon(FileTextureLoader.LoadTextureAsSprite(iconPath, out _))
+            .AsNonTransportableBuilding()
+            .WithPreferredPlacement(DefaultPreferredPlacementMode.Single);
+    }
+
+    public void Dispose() {}
 }
